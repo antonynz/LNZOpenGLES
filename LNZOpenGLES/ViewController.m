654ed4f9bd67213c88f480e2fa7632d77bc3b643
zeng <antonynz@foxmail.com>
2022-09-29 16:54:11 +0800

    feat: 设置 OpengGLES 上下文环境

--- LNZOpenGLES/ViewController.m
+++ LNZOpenGLES/ViewController.m
@@ -9,14 +9,43 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) EAGLContext *context;
+@property (nonatomic, assign) GLuint shaderProgram;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setupContext];
+    [self setupShader];
 }
 
+- (void)setupContext {
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+    if (self.context == nil) {
+        NSLog(@"Failed to create Context");
+    }
+    
+    GLKView *view = (GLKView *)self.view;
+    view.context = self.context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    [EAGLContext setCurrentContext:self.context];
+}
+
+- (void)setupShader {
+    
+}
+
+- (void)update {
+    
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(1.0, 0.2, 0.5, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(self.shaderProgram);
+}
 
 @end
